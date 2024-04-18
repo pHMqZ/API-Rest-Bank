bb510777de1e16f07a98e3fed466e5dc598eb2c3
Phillip Marques <phillipmarq@hotmail.com>
2024-04-18 17:00:20 -0300

    Implementing the application controllers and testing the endpoints

---
+++ src/main/java/com/pms/controller/dto/CardDTO.java
@@ -0,0 +1,20 @@
+package com.pms.controller.dto;
+
+import com.pms.domain.model.Card;
+
+import java.math.BigDecimal;
+
+public record CardDTO (Long id, String number, BigDecimal limit) {
+
+    public CardDTO(Card model){
+        this(model.getId(), model.getNumber(), model.getLimit());
+    }
+
+    public Card toModel() {
+        Card model = new Card();
+        model.setId(this.id);
+        model.setNumber(this.number);
+        model.setLimit(this.limit);
+        return model;
+    }
+}
---
+++ src/main/java/com/pms/controller/dto/NewsDTO.java
@@ -0,0 +1,19 @@
+package com.pms.controller.dto;
+
+import com.pms.domain.model.News;
+
+public record NewsDTO (Long id, String icon, String description){
+    public NewsDTO(News model){
+        this(model. getId(), model.getIcon(), model.getDescription());
+    }
+
+
+    public News toModel(){
+        News model = new News();
+        model.setId(this.id);
+        model.setIcon(this.icon);
+        model.setDescription(this.description);
+
+        return model;
+    }
+}
